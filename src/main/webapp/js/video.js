2d0ff2f21c7054a8ee06f0038edd6ce6b4296a0b
KCSiesta <396254277@qq.com>
2017-06-18 23:34:17 +0800

    video play

---
+++ src/main/webapp/js/video.js
@@ -0,0 +1,25 @@
+// videojs("ocean-play",{
+// 	"techOrder": ["html5","flash","other supported tech"],
+// 	"example_option":true,
+// 	"autoplay":true
+// },function(){
+           
+// });
+    // var myPlayer = videojs('ocean-play');
+    // videojs("ocean-play").ready(function(){
+    //     var myPlayer = this;
+    //     myPlayer.play();
+    // });
+
+ //    var myPlayer = videojs('ocean-play');
+ //    videojs("ocean-play", {
+ //    	techOrder: ["html5","flash","other supported tech"],
+	// autoplay:true
+ //    },(function(){
+ //        var myPlayer = this;
+ //        myPlayer.play();
+ //    });
+
+// videojs("ocean-play", {
+// 	example_option:true,
+// });
